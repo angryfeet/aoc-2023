c4ff099cf5bd2022ca40a5c1ea20874ec5b8b63f
Lyndsey Gardiner <lyndsey@lyndseygardiner.co.uk>
2023-12-10 07:10:34 +0000

    Day 9, part 2

--- day9.rb
+++ day9.rb
@@ -14,18 +14,39 @@ class Day9
     next_numbers.sum
   end
 
+  def part_2
+    first_numbers = []
+    @report.each do |line|
+      first_numbers << extrapolate_previous_number(line)
+    end
+    first_numbers.sum
+  end
+
   private
 
   def extrapolate_next_number(line)
-    steps = [line]
-    while steps.last.uniq.count != 1
-      steps << get_line_differences(steps.last)
-    end
+    steps = get_steps(line)
     steps.reverse.each_with_index do |step, index|
       next if index == 0
       step << step.last + steps.reverse[index - 1].last
     end
     steps.first.last
+  end
+
+  def extrapolate_previous_number(line)
+    steps = get_steps(line)
+    steps.reverse.each_with_index do |step, index|
+      step.unshift(step.first - steps.reverse[index - 1].first) if index != 0
+    end
+    steps.first.first
+  end
+
+  def get_steps(line)
+    steps = [line]
+    while steps.last.uniq.count != 1
+      steps << get_line_differences(steps.last)
+    end
+    steps
   end
 
   def get_line_differences(line)
@@ -40,3 +61,4 @@ end
 
 day_9 = Day9.new
 ap day_9.part_1
+ap day_9.part_2
